f594368dd02c42c240b600067e1747360ba6784b
Gabriel Nicolas Avellaneda <avellaneda.gabriel@gmail.com>
2015-05-16 23:11:44 -0300

    Initial version
    Signed-off-by: Gabriel Nicolas Avellaneda <avellaneda.gabriel@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# boundary-plugin-events
+Boundary Plugin Events
+----------------------
+
 Example plugin that shows how to emit events
+
+Example plugin that generates random events to show how to send different kinds of events to the boundary.com platform.
+
+### Prerequisites
+
+|     OS    | Linux | Windows | SmartOS | OS X |
+|:----------|:-----:|:-------:|:-------:|:----:|
+| Supported |   v   |    v    |    v    |  v   |
+
+- Boundary Meter version 4.0 or later _required_
+
+### Plugin Setup
+
+None
+
+#### Plugin Configuration Fields
+
+|Field Name|Description                         |
+|:---------|:-----------------------------------|
+|pollInterval | Interval between genertion of events |
+
+### Metrics Collected
+
+None
---
+++ init.lua
@@ -0,0 +1,44 @@
+-- Copyright 2015 Boundary, Inc.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--    http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.ocal framework = require('framework')
+
+local framework = require('framework')
+local Plugin = framework.Plugin
+local DataSource = framework.DataSource
+local notEmpty = framework.string.notEmpty
+local math = require('math')
+
+local params = framework.params
+params.name = 'Boundary Plugin Events'
+params.version = '1.0'
+params.tags = 'events'
+params.pollInterval = notEmpty(params.pollInterval, 5000)
+
+local randomEvent = (function ()
+  local events = {
+    'info', 'warn', 'error', 'critical'
+  }
+  return function () 
+    local idx = math.random(1, #events) 
+    return events[idx]
+  end
+end)()
+
+local ds = DataSource:new(randomEvent)
+
+local plugin = Plugin:new(params, ds)
+function plugin:onParseValues(data)
+  self:emitEvent(data, 'An ' .. data .. ' event.')
+end
+
+plugin:run()
